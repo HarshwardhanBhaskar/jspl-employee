1d37f27420527cf696d9c493c8baad058fc1231c
HarshwardhanBhaskar <hwbhaskar60147@gmail.com>
2025-06-04 22:15:56 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JSPL(Employee)',
+  webDir: 'www'
+};
+
+export default config;
